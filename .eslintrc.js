ba02152ef296acad86aa4b62d1ea933eac5dd43f
Fabian Brundke <fabian_brundke@gmx.net>
2019-11-18 14:53:15 +0100

    Added typescript extensions to .eslintrc.js for import/resolver settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,4 +25,11 @@ module.exports = {
   ],
   rules: {
   },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
 };
