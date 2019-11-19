b2d81b1811cd3b6858c46990b000aa264066b8ff
Fabian Brundke <fabian_brundke@gmx.net>
2019-11-19 10:09:01 +0100

    Brainstorming for graphql schema in schema/index.ts

---
+++ server/src/schema/index.ts
@@ -0,0 +1,19 @@
+// import required schemas for Projects and Packages
+
+/*
+# Fetch projects with following data:
+  - Date created
+  - Date modified
+  - Link to repo
+  - #deps
+  - #devDeps
+  - #outdated deps
+  - #outdated devDeps
+  - #security issues
+  - Description
+# Update individual project data
+# Fetch specific project data and list all dev/deps + comments
+
+# Check NPM registry for updates of utilized dev/deps
+# List all packages with usage statistics + versions
+*/
