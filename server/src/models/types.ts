ca97333d6b061cb00f0761eacbf87d658b0246bd
Fabian Brundke <fabian_brundke@gmx.net>
2019-11-22 21:56:46 +0100

    Defined type for Project.

--- server/src/models/types.ts
+++ server/src/models/types.ts
@@ -2,6 +2,20 @@
 import { Document } from 'mongoose';
 
 export interface ProjectSchemaType extends Document {
+  title: string;
+  dateCreated: string;
+  dateModified: string;
+  repositoryLink: string;
+  numberDependencies: number;
+  numberDevDependencies: number;
+  numberOutdatedDependencies: number;
+  numberOutdatedDevDependencies: number;
+  numberSecurityIssues: number;
+  description: string;
+}
+
+export interface ProjectType {
+  title: string;
   dateCreated: string;
   dateModified: string;
   repositoryLink: string;
