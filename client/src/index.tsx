612a978996a013c27ece4654d8f73d9edbe9f8a9
Fabian Brundke <fabian_brundke@gmx.net>
2019-12-07 20:56:31 +0100

    Defined sample graphql query from client.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+client
+  .query({
+    query: gql`
+      query Projects {
+        projects {
+          title
+        }
+      }
+    `,
+  })
+  .then((result: unknown) => console.log(result));
 
 ReactDOM.render(
   <div>
