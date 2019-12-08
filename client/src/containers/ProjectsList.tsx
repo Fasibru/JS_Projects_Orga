44bd366e05cbd28b62f8bd4f4679b9f58437b1b1
Fabian Brundke <fabian_brundke@gmx.net>
2019-12-08 21:01:43 +0100

    Removed unused imports.

--- client/src/containers/ProjectsList.tsx
+++ client/src/containers/ProjectsList.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import gql from 'graphql-tag';
 
 import Pages from './pages/index';
 
